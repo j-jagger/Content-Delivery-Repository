13a073b034dee8e218dc350e744f4fde51ca8e95
j-jagger <joejaggermain@outlook.com>
2025-10-28 22:59:06 +0000

    Added Docker Installscript [ub]

---
+++ bash/docker.sh
@@ -0,0 +1,25 @@
+# Scriptified Docker Install [UBUNTU]
+
+
+echo "DOCKER INSTALL SCRIPT STARTED"
+
+sudo -i
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do  apt-get remove $pkg; done
+
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+ apt-get update -y
+
+ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+echo "DOCKER INSTALL SCRIPT FINISHED"
+systemctl status docker
